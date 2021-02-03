6ba333bfdbf339f73c4e13be4e133717b045b783
Lucas dos Santos de Oliveira <lucax.oliveira96@gmail.com>
2021-02-03 15:32:27 -0300

    projetinho completo

---
+++ script.js
@@ -0,0 +1,71 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false;
+let position = 0;
+
+function handleKeyUp(event){
+	if(event.keyCode === 32){
+		// console.log('pressionou o espaço');
+		if(!isJumping){
+			jump();
+		}
+	}
+}
+
+function jump() {
+	isJumping  = true;
+	
+	let upInterval = setInterval(() => {
+		if(position >= 150){
+			clearInterval(upInterval);
+			//descendo
+			
+			let downInterval = setInterval(() => {
+				if(position <= 0){
+					clearInterval(downInterval);
+					isJumping=false;
+				}else {
+					position -= 20;
+					dino.style.bottom = position + 'px';
+				}
+			}, 20);
+		}else{
+			position += 20;
+			dino.style.bottom = position + 'px';
+		}
+	}, 20);
+}
+
+function createCactus(){
+	const cactus = document.createElement('div');
+	let cactusPosition = 1000;
+	let randomTime = Math.random() * 6000;
+
+	// console.log(randomTime);
+
+	cactus.classList.add('cactus');	
+	background.appendChild(cactus);
+	cactus.style.left = cactusPosition + 'px';
+
+
+	let leftInterval = setInterval(() => {
+
+		if(cactusPosition < -60) {
+			clearInterval(leftInterval);
+			background.removeChild(cactus);
+		}else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+		//
+			clearInterval(leftInterval);
+			document.body.innerHTML = '<h1 class="game-over">fim de jogo</h1>';
+		}else{
+			cactusPosition -= 10;
+			cactus.style.left = cactusPosition + 'px';
+		}
+	}, 20);
+
+	setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
